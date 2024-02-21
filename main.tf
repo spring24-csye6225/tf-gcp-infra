9fc89f13ff9f59246c2cdec2f2d22fe697c8d474
Vakiti98 <vakitisaikumar1998@gmail.com>
2024-02-21 14:41:12 -0500

    added Vm config

--- main.tf
+++ main.tf
@@ -31,3 +31,46 @@ resource "google_compute_route" "webapp_route" {
   network          = google_compute_network.vpc_network.self_link
   next_hop_gateway = "default-internet-gateway"
 }
+
+resource "google_compute_instance" "my-web-server" {
+  name         = "web-server"    # Updated name
+  zone         = "us-central1-a" # Adjust zone if needed
+  machine_type = "e2-medium"     # Adjust machine type if needed
+  tags         = ["web-server"]
+
+  boot_disk {
+    auto_delete = true
+    device_name = "my-web-server" # Align device name with instance 
+    initialize_params {
+      image = "projects/vakiti-dev/global/images/my-app-image-1708543596"
+      size  = 20
+      type  = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.webapp_subnet.name
+    access_config {
+      network_tier = "PREMIUM"
+      # Assign a public IP if needed
+    }
+  }
+
+  # ... other configuration: service_account, shielded_instance_config, tags (consider variables here as well)
+}
+
+
+resource "google_compute_firewall" "allow-web-traffic" {
+  name        = "allow-web-traffic"
+  network     = google_compute_network.vpc_network.name // Reference your VPC network 
+  description = "Allow HTTP traffic to instances with the 'web-server' tag"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080", "22"]
+  }
+
+  target_tags   = ["web-server"]
+  source_ranges = ["0.0.0.0/0"] // Allow access from anywhere (public internet)
+}
+
